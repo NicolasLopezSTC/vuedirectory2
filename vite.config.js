0956d7f1dd4e110205f891768b4ae0fd044a0c4d
NicolasLopezSTC <gclan149@gmail.com>
2023-12-11 17:34:49 -0600

    new directory
    Signed-off-by: NicolasLopezSTC <gclan149@gmail.com>

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/VueDirectory/',
+  base: '/VueDirectory2/',
   plugins: [vue()],
   resolve: {
     alias: {
