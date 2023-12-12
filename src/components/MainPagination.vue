6b9a2e5a6b83ebc9811ea1577b4a42ca0c6e1087
NicolasLopezSTC <gclan149@gmail.com>
2023-12-11 22:41:49 -0600

    mian page needs fixing
    Signed-off-by: NicolasLopezSTC <gclan149@gmail.com>

--- src/components/MainPagination.vue
+++ src/components/MainPagination.vue
@@ -1,17 +1,23 @@
 <script setup>
-  import { ref } from 'vue'
-  const pages = ref(10)
-  const activePage = ref(4)
+  import useAPI from '@/composables/useAPI'
 
-  const prevPage = () => {
+  const { activePage, pages, getEmployees } = useAPI()
+
+  const prevPage = async () => {
     if (activePage.value > 1) {
       activePage.value--
+      await getEmployees()
     }
   }
-  const nextPage = () => {
+  const nextPage = async () => {
     if (activePage.value < pages.value) {
       activePage.value++
+      await getEmployees()
     }
+  }
+  const jumpPage = async (page) => {
+    activePage.value = page
+    await getEmployees()
   }
 </script>
 
@@ -23,23 +29,24 @@
       :key="page"
       class="page"
       :class="page === activePage ? 'active' : ''"
-      @click="activePage = page"
+      @click="jumpPage(page)"
     >
       {{ page }}
     </button>
     <button class="action" :disabled="activePage === pages" @click="nextPage">Next</button>
   </div>
 </template>
+
 <style lang="postcss" scoped>
   .pagination {
     @apply flex justify-center gap-4;
     .action {
-      @apply rounded-md bg-green-100 p-2 font-medium text-purple-700 shadow-md hover:bg-slate-200 disabled:text-slate-400 hover:disabled:bg-slate-100;
+      @apply rounded-md bg-slate-100 p-2 font-medium text-slate-700 shadow-md hover:bg-slate-200 disabled:text-slate-400 hover:disabled:bg-slate-100;
     }
     .page {
-      @apply rounded-md bg-slate-100 p-2 font-medium text-purple-700 shadow-md hover:bg-slate-200;
+      @apply rounded-md bg-slate-100 p-2 font-medium text-slate-700 shadow-md hover:bg-slate-200;
       &.active {
-        @apply bg-red-700 text-slate-100 hover:bg-yellow-600;
+        @apply bg-yellow-700 text-slate-100 hover:bg-yellow-600;
       }
     }
   }
