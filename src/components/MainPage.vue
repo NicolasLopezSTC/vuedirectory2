ea40da7ea6c45fd1b5e2cd71fd8681f86adb6653
NicolasLopezSTC <gclan149@gmail.com>
2023-12-11 22:33:51 -0600

    missing main page
    Signed-off-by: NicolasLopezSTC <gclan149@gmail.com>

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,13 +1,22 @@
 <script setup>
-import { onMounted } from 'vue'
+  import { onMounted } from 'vue'
 
-import MainSearch from '@/components/MainSearch.vue'
-import MainCards from '@/components/MainCards.vue'
-import MainPagination from '@/components/MainPagination.vue'
+  import MainSearch from '@/components/MainSearch.vue'
+  import MainCards from '@/components/MainCards.vue'
+  import MainPagination from '@/components/MainPagination.vue'
+
+  import useAPI from '@/composables/useAPI'
+
+  const { getEmployees, loading } = useAPI()
+
+  onMounted(async () => {
+    await getEmployees()
+  })
 </script>
 
 <template>
-  <div class="wrapper">
+  <p v-if="loading">Loading...</p>
+  <div v-else class="wrapper">
     <MainSearch />
     <MainCards />
     <MainPagination />
@@ -15,7 +24,7 @@ import MainPagination from '@/components/MainPagination.vue'
 </template>
 
 <style scoped lang="postcss">
-.wrapper {
-  @apply container mx-auto flex flex-col gap-4 py-4;
-}
+  .wrapper {
+    @apply container mx-auto flex flex-col gap-4 py-4;
+  }
 </style>
